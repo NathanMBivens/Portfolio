6a745ceb886a8e18ab3a9ead0ca2225d24837a74
Nathan M. Bivens <nathanmbivens@gmail.com>
2017-11-20 10:57:00 -0600

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NB_Portfolio from './FINAL_COMPONENTS/NB_Portfolio';
-import style from './styles/style.scss';
-ReactDOM.render(<NB_Portfolio/>, document.getElementById('app'));
